2ffca3acaec506a7d20f1337f8c897073b97b056
Administrator <Administrator@F17PCD2255>
2018-12-03 11:59:32 -0600

    Added some stuff.

--- TestContainer/TestContainer/MainPage.xaml.cs
+++ TestContainer/TestContainer/MainPage.xaml.cs
@@ -9,11 +9,20 @@ namespace TestContainer
 {
     public partial class MainPage : ContentPage
     {
-
+        bool t = true;
 
         public MainPage()
         {
             InitializeComponent();
+
+            List<string> pageList = new List<string>
+            {
+                "p1.jpg",
+                "p2.jpg",
+                "p3.jpg"
+            };
+        
+            view.ItemsSource = pageList;
         }
 
         protected override void OnSizeAllocated(double width, double height)
@@ -21,6 +30,19 @@ namespace TestContainer
             base.OnSizeAllocated(width, height);
             App.ScreenWidth = width;
             App.ScreenHeight = height;
+
+            double _width;
+            double _height;
+
+
+            if( width > height & t == true)
+            {
+                _width = width;
+                _height = height;
+                t = false;
+                ForceLayout();
+            }
+            
         }
     }
 }
