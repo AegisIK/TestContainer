b3f73f5de00b3ad9b4a939fee12b739ec657e15c
Kevin Han <main.share.us@gmail.com>
2018-12-13 22:08:31 -0600

    Differentiate DispatchTouchEvent and Scaling. Fix translation jankiness.

--- TestContainer/TestContainer.Android/ZoomScrollViewRenderer.cs
+++ TestContainer/TestContainer.Android/ZoomScrollViewRenderer.cs
@@ -25,7 +25,17 @@ namespace TestContainer.Droid
     public class ZoomScrollViewRenderer : ScrollViewRenderer, IOnScaleGestureListener
     {
         private float mScale = 1f;
+        private static int INVALID_POINTER_ID = 999;
+        private int mActivePointerId = INVALID_POINTER_ID;
+
+        private float mLastTouchX;
+        private float mLastTouchY;
+
+        private float mPosX = 0;
+        private float mPosY = 0;
+
         private ScaleGestureDetector mScaleDetector;
+
 
         protected override void OnElementChanged(VisualElementChangedEventArgs e)
         {
@@ -35,10 +45,75 @@ namespace TestContainer.Droid
 
         }
 
-
+        
         public override bool DispatchTouchEvent(MotionEvent e)
         {
             base.DispatchTouchEvent(e);
+
+            MotionEventActions action = e.ActionMasked;
+
+            switch(action)
+            {
+                case MotionEventActions.Down:
+                    int pointerIndex = e.ActionIndex;
+                    float x = e.GetX(pointerIndex);
+                    float y = e.GetY(pointerIndex);
+
+                    //Remember where we started (for dragging)
+                    mLastTouchX = x;
+                    mLastTouchY = y;
+
+                    //Save the ID of this pointer (for dragging)
+                    mActivePointerId = e.GetPointerId(0);
+                    break;
+
+                case MotionEventActions.Move:
+                    //Find the index of the active pointer and fetch its position
+                    int activePointerIndex = e.GetPointerId(mActivePointerId);
+
+                    float x2 = e.GetX(activePointerIndex);
+                    float y2 = e.GetY(activePointerIndex);
+
+                    //Calculate the distance moved
+                    float dx = x2 - mLastTouchX;
+                    float dy = y2 - mLastTouchY;
+
+                    mPosX += dx;
+                    mPosY += dy;
+
+                    TranslateAnimation translateAnimation = new TranslateAnimation(mPosX, dx, mPosY, dy);
+                    translateAnimation.Duration = 0;
+                    translateAnimation.FillAfter = true;
+                    StartAnimation(translateAnimation);
+                    //Remember this touch position for the next move event
+                    mLastTouchX = x2;
+                    mLastTouchY = y2;
+                    break;
+
+                case MotionEventActions.Up:
+                    mActivePointerId = INVALID_POINTER_ID;
+                    break;
+
+                case MotionEventActions.Cancel:
+                    mActivePointerId = INVALID_POINTER_ID;
+                    break;
+
+                case MotionEventActions.PointerUp:
+                    int pointerIndex2 = e.ActionIndex;
+                    int pointerId2 = e.GetPointerId(pointerIndex2);
+
+                    if(pointerId2 == mActivePointerId)
+                    {
+                        //This was our active pointer going up. Choose a new active pointer and adjust accordingly
+                        int newPointerIndex = pointerIndex2 == 0 ? 1 : 0;
+                        mLastTouchX = e.GetX(newPointerIndex);
+                        mLastTouchY = e.GetY(newPointerIndex);
+                        mActivePointerId = e.GetPointerId(newPointerIndex);
+                    
+                    }
+                    break;
+            }
+            
             return mScaleDetector.OnTouchEvent(e);
         }
 
